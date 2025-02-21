707fa8e4869e22637be74aa4d8aa84c0c53d7d2d
juliomr21 <juliomr21.correo@gmail.com>
2025-02-21 00:04:48 -0300

    cors

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 origins = [
     "http://localhost:4200",  # Angular en desarrollo
     "http://127.0.0.1:4200",  # Otra variante
+    "https://juliomr21.github.io"
 ]
 
 # Agregar middleware CORS
