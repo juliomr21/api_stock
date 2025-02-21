94c235faac33aa14740795353d5e6d623cc2ec3c
juliomr21 <juliomr21.correo@gmail.com>
2025-02-21 01:01:29 -0300

    BD

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+# MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = 'mongodb+srv://juliomr21correo:1yw0eZzoJiRFVuxn@cluster0.pcse6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["stock_db"]
